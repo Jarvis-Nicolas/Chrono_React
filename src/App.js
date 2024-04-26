69610e19a2704582e3f3589ea0c586c422f02d1e
JARVIS NICOLAS <jarvisalix@gmail.com>
2024-04-26 18:47:32 +0200

    JS et CCS fait

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevtime) => prevtime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Chronomètre React JS</h1>
+      <div class="container">
+        <div class="timer-display">
+          <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+          <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+          <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+        </div>
+        <div class="buttons">
+          {running ? (
+            <button onClick={() => { setRunning(false) }}>Stop</button>
+          ) : (
+            <button onClick={() => { setRunning(true) }}>Start</button>
+          )}
+          <button onClick={() => { setTime(0) }}>Reset</button>
+        </div>
+      </div>
+    </>
   );
 }
 
